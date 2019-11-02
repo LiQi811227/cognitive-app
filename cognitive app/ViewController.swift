0f3332ccf45f50dd6491ee21d1b745c758008a52
liqi <qili811227@gmail.com>
2019-11-03 00:00:34 +1300

    Initial Commit

---
+++ cognitive app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cognitive app
+//
+//  Created by LiQi on 2/11/19.
+//  Copyright © 2019 LiQi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
